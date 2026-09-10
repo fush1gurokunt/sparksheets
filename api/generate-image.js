4ad4e49e46c9b64645621c149de88168cc08109c
fush1gurokunt <rdwyer47@gmail.com>
2026-09-10 11:55:01 +0100

    Update model in generate-image.js to gpt-image-2.5

--- api/generate-image.js
+++ api/generate-image.js
@@ -23,11 +23,7 @@ module.exports = async (req, res) => {
         'Authorization': `Bearer ${apiKey}`,
       },
       body: JSON.stringify({
-        model: 'dall-e-3',
-        prompt,
-        n: 1,
-        size: '1024x1024',
-        response_format: 'url',
+        model: 'gpt-image-2.5-sunburst', prompt, n: 1, size: '1024x1024',
       }),
     });
   } catch (err) {
